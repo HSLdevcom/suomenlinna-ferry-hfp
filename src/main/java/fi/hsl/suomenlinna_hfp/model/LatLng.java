afba728376d3e177f19b05989407b0cad15f2ae7
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2020-02-04 13:34:54 +0200

    Create LatLng class for geographic coordinates

---
+++ src/main/java/fi/hsl/suomenlinna_hfp/model/LatLng.java
@@ -0,0 +1,70 @@
+package fi.hsl.suomenlinna_hfp.model;
+
+import java.util.Objects;
+
+public class LatLng {
+    private static final int EARTH_RADIUS_IN_METERS = 6371 * 1000;
+
+    private double latitude;
+    private double longitude;
+
+    public LatLng() {
+    }
+
+    public LatLng(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    /**
+     * Calculates distance in meters between coordinates
+     * @param other Coordinates of the other location
+     * @return Distance between locations in meters
+     */
+    public double distanceTo(LatLng other) {
+        double latDistance = Math.toRadians(other.getLatitude() - latitude);
+        double lonDistance = Math.toRadians(other.getLongitude() - longitude);
+
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(latitude)) * Math.cos(Math.toRadians(other.getLatitude()))
+                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        return EARTH_RADIUS_IN_METERS * c;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LatLng latLng = (LatLng) o;
+        return Double.compare(latLng.latitude, latitude) == 0 &&
+                Double.compare(latLng.longitude, longitude) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(latitude, longitude);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + latitude + ", " + longitude + ")";
+    }
+}
---
+++ src/test/java/fi/hsl/suomenlinna_hfp/model/LatLngTest.java
@@ -0,0 +1,15 @@
+package fi.hsl.suomenlinna_hfp.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LatLngTest {
+    @Test
+    public void testDistanceTo() {
+        LatLng from = new LatLng(60.1992303,24.9407805); //Opastinsilta 6A
+        LatLng to = new LatLng(60.1987249,24.933365); //Pasila station
+
+        assertEquals(413, from.distanceTo(to), 1);
+    }
+}
