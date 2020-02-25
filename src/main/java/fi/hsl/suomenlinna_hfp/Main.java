ab21ed99592d7b515327843824c79e8c2dd44e91
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2020-02-25 10:51:16 +0200

    Simplify Main class

--- src/main/java/fi/hsl/suomenlinna_hfp/Main.java
+++ src/main/java/fi/hsl/suomenlinna_hfp/Main.java
@@ -14,28 +14,23 @@ import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
-    private static final List<String> SUOMENLINNA_FERRY_ROUTES = Arrays.asList("1019", "1019E");
-
-    private static final Map<String, VehicleId> SUOMENLINNA_FERRY_IDS;
-
-    static {
-        Map<String, VehicleId> ids = new HashMap<>(4);
-        ids.put( "230985490", new VehicleId(60, 2)); //SUOMENLINNA II
-        ids.put("230024660", new VehicleId(60, 3)); //TOR
-        ids.put("230108610", new VehicleId(60, 4)); //SUOKKI
-        ids.put("230108590", new VehicleId(60, 5)); //EHRENSVÄRD
-
-        SUOMENLINNA_FERRY_IDS = Collections.unmodifiableMap(ids);
-    }
-
     public static void main(String[] args) throws Throwable {
+        //TODO: allow configuring these with env variables
+        List<String > suomenlinnaFerryRoutes = Arrays.asList("1019", "1019E");
+
+        Map<String, VehicleId> suomenlinnaFerryIds = new HashMap<>();
+        suomenlinnaFerryIds.put( "230985490", new VehicleId(60, 2)); //SUOMENLINNA II
+        suomenlinnaFerryIds.put("230024660", new VehicleId(60, 3)); //TOR
+        suomenlinnaFerryIds.put("230108610", new VehicleId(60, 4)); //SUOKKI
+        suomenlinnaFerryIds.put("230108590", new VehicleId(60, 5)); //EHRENSVÄRD
+
         Map<String, Double> maxDistanceFromStop = new HashMap<>();
         maxDistanceFromStop.put("1080701", 45.0);
         maxDistanceFromStop.put("1520703", 35.0);
 
         double defaultMaxDistanceFromStop = 25;
 
-        TripProcessor tripProcessor = new TripProcessor(ZoneId.of("Europe/Helsinki"), SUOMENLINNA_FERRY_ROUTES, maxDistanceFromStop, defaultMaxDistanceFromStop,
+        TripProcessor tripProcessor = new TripProcessor(ZoneId.of("Europe/Helsinki"), suomenlinnaFerryRoutes, maxDistanceFromStop, defaultMaxDistanceFromStop,
                 Duration.of(2, ChronoUnit.MINUTES), //Allow registering for a trip up to 2 minutes before scheduled departure
                 Duration.of(5, ChronoUnit.MINUTES), //Allow registering for a trip up to 5 minutes after scheduled departure
                 Duration.of(15, ChronoUnit.MINUTES)); //Allow registering for next trip if the previous trip is not finished within 15 minutes of scheduled arrival to the final stop
@@ -44,6 +39,6 @@ public class Main {
         GtfsProvider gtfsProvider = new HttpGtfsProvider("https://dev.hsl.fi/gtfs/hsl.zip", 12, TimeUnit.HOURS);
         MqttHfpPublisher mqttHfpPublisher = new MqttHfpPublisher("tcp://hsl-mqtt-lab-d.cinfra.fi:1883");
 
-        new SuomenlinnaHfpProducer(SUOMENLINNA_FERRY_IDS, tripProcessor, gtfsProvider, vesselLocationProvider, mqttHfpPublisher).run();
+        new SuomenlinnaHfpProducer(suomenlinnaFerryIds, tripProcessor, gtfsProvider, vesselLocationProvider, mqttHfpPublisher).run();
     }
 }
