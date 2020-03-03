99ca7af45daffd1aed00136804961f13c795801b
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2020-03-03 12:46:53 +0200

    Parse config from file

--- src/main/java/fi/hsl/suomenlinna_hfp/Main.java
+++ src/main/java/fi/hsl/suomenlinna_hfp/Main.java
@@ -1,5 +1,7 @@
 package fi.hsl.suomenlinna_hfp;
 
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 import fi.hsl.suomenlinna_hfp.digitraffic.provider.MqttVesselLocationProvider;
 import fi.hsl.suomenlinna_hfp.digitraffic.provider.VesselLocationProvider;
 import fi.hsl.suomenlinna_hfp.gtfs.provider.GtfsProvider;
@@ -12,32 +14,39 @@ import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws Throwable {
-        //TODO: allow configuring these with env variables
-        List<String> suomenlinnaFerryRoutes = Arrays.asList("1019", "1019E");
+        Config config = ConfigFactory.load();
 
-        Map<String, VehicleId> suomenlinnaFerryIds = new HashMap<>();
-        suomenlinnaFerryIds.put( "230985490", new VehicleId(60, 2)); //SUOMENLINNA II
-        suomenlinnaFerryIds.put("230024660", new VehicleId(60, 3)); //TOR
-        suomenlinnaFerryIds.put("230108610", new VehicleId(60, 4)); //SUOKKI
-        suomenlinnaFerryIds.put("230108590", new VehicleId(60, 5)); //EHRENSVÄRD
+        List<String> suomenlinnaFerryRoutes = config.getStringList("routes");
 
-        Map<String, Double> maxDistanceFromStop = new HashMap<>();
-        maxDistanceFromStop.put("1080701", 45.0);
-        maxDistanceFromStop.put("1520703", 35.0);
+        Map<String, VehicleId> suomenlinnaFerryIds = config.getConfigList("mmsiToVehicleId").stream().collect(Collectors.toMap(c -> c.getString("mmsi"), c -> new VehicleId(c.getInt("operator"), c.getInt("vehicle"))));
 
-        double defaultMaxDistanceFromStop = 25;
+        double defaultMaxDistanceFromStop = config.getDouble("defaultMaxDistanceFromStop");
 
-        TripProcessor tripProcessor = new TripProcessor(ZoneId.of("Europe/Helsinki"), suomenlinnaFerryRoutes, maxDistanceFromStop, defaultMaxDistanceFromStop,
+        Map<String, Double> maxDistanceFromStop = config.getObject("maxDistanceFromStop").entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, entry -> ((Number)entry.getValue().unwrapped()).doubleValue()));
+
+        ZoneId timezone = ZoneId.of(config.getString("timezone"));
+
+        String meriDigitrafficBroker = config.getString("meriDigitraffic.broker");
+        String meriDigitrafficUser = config.getString("meriDigitraffic.user");
+        String meriDigitrafficPassword = config.getString("meriDigitraffic.password");
+
+        String gtfsUrl = config.getString("gtfs.url");
+        Duration gtfsPollInterval = config.getDuration("gtfs.pollInterval");
+
+        String publisherBroker = config.getString("publisher.broker");
+
+        TripProcessor tripProcessor = new TripProcessor(timezone, suomenlinnaFerryRoutes, maxDistanceFromStop, defaultMaxDistanceFromStop,
                 Duration.of(4, ChronoUnit.MINUTES), //Allow registering for a trip up to 4 minutes before scheduled departure
                 Duration.of(5, ChronoUnit.MINUTES), //Allow registering for a trip up to 5 minutes after scheduled departure
                 Duration.of(15, ChronoUnit.MINUTES)); //Allow registering for next trip if the previous trip is not finished within 15 minutes of scheduled arrival to the final stop
 
-        VesselLocationProvider vesselLocationProvider = new MqttVesselLocationProvider("wss://meri.digitraffic.fi:61619/mqtt", "digitraffic", "digitrafficPassword");
-        GtfsProvider gtfsProvider = new HttpGtfsProvider("https://dev.hsl.fi/gtfs/hsl.zip", 12, TimeUnit.HOURS);
-        MqttHfpPublisher mqttHfpPublisher = new MqttHfpPublisher("tcp://hsl-mqtt-lab-d.cinfra.fi:1883");
+        VesselLocationProvider vesselLocationProvider = new MqttVesselLocationProvider(meriDigitrafficBroker, meriDigitrafficUser, meriDigitrafficPassword);
+        GtfsProvider gtfsProvider = new HttpGtfsProvider(gtfsUrl, gtfsPollInterval.getSeconds(), TimeUnit.SECONDS);
+        MqttHfpPublisher mqttHfpPublisher = new MqttHfpPublisher(publisherBroker);
 
         new SuomenlinnaHfpProducer(suomenlinnaFerryRoutes, suomenlinnaFerryIds, tripProcessor, gtfsProvider, vesselLocationProvider, mqttHfpPublisher).run();
     }
