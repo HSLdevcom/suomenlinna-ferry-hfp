9fab6a26d9d833cbdd0e2d391d4c5bc7bd910864
Aki Väliaho <aki.valiaho@hsl.fi>
2020-04-03 15:49:33 +0300

    Revert "Suspending debug point"
    This reverts commit 90e0b4d4

--- start-application.sh
+++ start-application.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "${DEBUG_ENABLED}" = true ]]; then
-  java -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005 -jar /app/application.jar
+  java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar /app/application.jar
 else
   java -jar /app/application.jar
 fi
