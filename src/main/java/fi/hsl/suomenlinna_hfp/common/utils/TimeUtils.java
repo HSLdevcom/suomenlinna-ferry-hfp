bd357c035bb1cab802489a9998a02d502d259c27
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2020-07-10 13:10:21 +0300

    Create methods for parsing GTFS times to LocalDateTime

--- src/main/java/fi/hsl/suomenlinna_hfp/common/utils/TimeUtils.java
+++ src/main/java/fi/hsl/suomenlinna_hfp/common/utils/TimeUtils.java
@@ -1,7 +1,14 @@
 package fi.hsl.suomenlinna_hfp.common.utils;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class TimeUtils {
     private TimeUtils() {}
+
+    private static final int ONE_DAY_IN_SECONDS = 24 * 60 * 60;
 
     /**
      * Parses time in HH:mm:ss format to seconds
@@ -14,5 +21,27 @@ public class TimeUtils {
             throw new IllegalArgumentException("Invalid time format: "+gtfsTime);
         }
         return Integer.parseInt(parts[0]) * 60 * 60 + Integer.parseInt(parts[1]) * 60 + Integer.parseInt(parts[2]);
+    }
+
+    /**
+     * Converts GTFS time to LocalDateTime
+     * @param startDate Start date in format YYYYMMDD
+     * @param startTime Start time in format HH:mm:ss
+     */
+    public static LocalDateTime gtfsTimeToLocalDateTime(String startDate, String startTime) {
+        final LocalDate date = LocalDate.parse(startDate, DateTimeFormatter.BASIC_ISO_DATE);
+        final int startTimeInSeconds = parseTime(startTime);
+
+        return gtfsTimeToLocalDateTime(date, startTimeInSeconds);
+    }
+
+    /**
+     * Converts GTFS time to LocalDateTime
+     */
+    public static LocalDateTime gtfsTimeToLocalDateTime(LocalDate startDate, int startTime) {
+        final int startTimeAfterMidnight = startTime % ONE_DAY_IN_SECONDS; //GTFS time can be over 24 hours, e.g. 28:00:00 would be 4am
+        final int startDaysAfterDate = startTime / ONE_DAY_IN_SECONDS; //Find actual start date
+
+        return startDate.plusDays(startDaysAfterDate).atTime(LocalTime.ofSecondOfDay(startTimeAfterMidnight));
     }
 }
--- src/test/java/fi/hsl/suomenlinna_hfp/common/utils/TimeUtilsTest.java
+++ src/test/java/fi/hsl/suomenlinna_hfp/common/utils/TimeUtilsTest.java
@@ -2,6 +2,10 @@ package fi.hsl.suomenlinna_hfp.common.utils;
 
 import org.junit.Test;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 import static org.junit.Assert.assertEquals;
 
 public class TimeUtilsTest {
@@ -16,5 +20,20 @@ public class TimeUtilsTest {
     @Test(expected = IllegalArgumentException.class)
     public void testParseTimeWithInvalidValue() {
         TimeUtils.parseTime("test");
+    }
+
+    @Test
+    public void testGtfsTimeToLocalDateTime() {
+        LocalDateTime localDateTime1 = TimeUtils.gtfsTimeToLocalDateTime("20200101", "09:00:00");
+        assertEquals(LocalDate.of(2020, 1, 1), localDateTime1.toLocalDate());
+        assertEquals(LocalTime.of(9, 0), localDateTime1.toLocalTime());
+
+        LocalDateTime localDateTime2 = TimeUtils.gtfsTimeToLocalDateTime("20200101", "28:00:00");
+        assertEquals(LocalDate.of(2020, 1, 2), localDateTime2.toLocalDate());
+        assertEquals(LocalTime.of(4, 0), localDateTime2.toLocalTime());
+
+        LocalDateTime localDateTime3 = TimeUtils.gtfsTimeToLocalDateTime("20200101", "50:00:00");
+        assertEquals(LocalDate.of(2020, 1, 3), localDateTime3.toLocalDate());
+        assertEquals(LocalTime.of(2, 0), localDateTime3.toLocalTime());
     }
 }
