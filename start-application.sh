a1a40bbb7ff63ed6b8f2006534a68e35471a9b3f
Jaakko Malkki <32361480+mjaakko@users.noreply.github.com>
2022-12-07 10:53:48 +0200

    Fix broken startup script in prod

--- start-application.sh
+++ start-application.sh
@@ -3,5 +3,5 @@
 if [[ "${DEBUG_ENABLED}" = true ]]; then
   java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 -XX:InitialRAMPercentage=10.0 -XX:MaxRAMPercentage=95.0 -jar /app/application.jar
 else
-  java -XX:InitialRAMPercentage=10.0", "-XX:MaxRAMPercentage=95.0 -jar /app/application.jar
+  java -XX:InitialRAMPercentage=10.0 -XX:MaxRAMPercentage=95.0 -jar /app/application.jar
 fi
