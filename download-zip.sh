d681ad2c3b917e79ac2e0c5fda054f5c30c1864e
Timo Järvinen <timo.jarvinen@yahoo.com>
2025-02-28 17:01:46 +0200

    Modify zip file path

--- download-zip.sh
+++ download-zip.sh
@@ -5,8 +5,8 @@ wget -O hsl-new.zip https://dev.hsl.fi/gtfs/hsl.zip
 
 echo "Replacing old GTFS data with new data..."
 ls -la hsl*
-rm hsl.zip
-mv hsl-new.zip hsl.zip
+rm /hsl.zip
+mv hsl-new.zip /hsl.zip
 
 echo "Downloading new HSL GTFS data completed."
 ls -la hsl*
